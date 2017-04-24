172d2653345eecd7a05d612c3b9baf769e7c7b9a
Hiromitsu Yamauchi <info@oyoyo-project.com>
2017-04-24 18:37:58 +0900

    ws test

--- app/assets/javascripts/components/stream.jsx
+++ app/assets/javascripts/components/stream.jsx
@@ -13,7 +13,7 @@ const createWebSocketURL = (url) => {
 export default function getStream(streamingAPIBaseURL, accessToken, stream, { connected, received, disconnected, reconnected }) {
   const ws = new WebSocketClient(`${createWebSocketURL(streamingAPIBaseURL)}/api/v1/streaming/?access_token=${accessToken}&stream=${stream}`);
 
-  console.log(`${createWebSocketURL(streamingAPIBaseURL)}/api/v1/streaming/?access_token=${accessToken}&stream=${stream}`):
+  console.log(`${createWebSocketURL(streamingAPIBaseURL)}/api/v1/streaming/?access_token=${accessToken}&stream=${stream}`);
   ws.onopen      = connected;
   ws.onmessage   = e => received(JSON.parse(e.data));
   ws.onclose     = disconnected;
